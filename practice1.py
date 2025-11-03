cbfc2cf4c047756c1f8eda2cfc032dd52044e3eb
raj4944 <raj4944@gmail.com>
2025-11-03 23:04:24 +0530

    V1

---
+++ practice1.py
@@ -0,0 +1,4 @@
+#write a program to input 2 numbers and print their sum.
+first = int(input("enter your first number: "))
+second = int(input("Enter your second number: "))
+print("sum = ", first+second)
