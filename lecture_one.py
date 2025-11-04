686face9ae3c4657ad8e609f2e147f480187dfa5
raj4944 <raj4944@gmail.com>
2025-11-04 21:04:42 +0530

    V4

---
+++ lecture_one.py
@@ -0,0 +1,88 @@
+name = "Rajesh"
+age = 23
+price = 35.56
+age2 = age
+size= "6-inch"
+print("My name is : ", name)
+print("My age is : ", age)
+print("my coat price is :", price)
+print("My current age is : ", age2)
+print("My shoe size is : ", size)
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(size))
+a = 29
+b = 50
+sum=a+b
+print(sum)
+n = 56 
+m = 76
+print(n+m)
+print(n-m)
+print(n*m)
+print(n/m)
+print(n%m)
+print(n**m)
+print(25+50)
+print(23*12)
+k = 60
+l = 75
+print(k == l)
+print(k!=l)
+print(k<=l)
+print(l>=k)
+#assignment operator
+num = 10
+num = num+10
+num = 20
+num+= 10
+num=59
+num-=67
+num =num-28
+print("num :", num)
+#logical operator
+a = 37
+b = 24
+print(not False)
+print(not (a>b))
+#and operator
+val1 = True
+val2 = True
+print("Ans operator:", val1 and val2)
+val3 = True
+val4 = False
+print("Ans operator :", val3 and val4)
+val1 = True
+val2 = True
+print("Ans operator:", val1 or val2)
+val3 = True
+val4 = False
+print("Ans operator :", val3 or val4)
+print("OR operator:", (a == b) or (a>b))
+a = (3)
+b = (5.5)
+sum = a+b
+print(type(a))
+print(type(b))
+print(sum)
+#input values
+name = input("Enter your name: ")
+print("Welcome", name)
+print("How can i Help you?")
+age = input("Enter your Age: ")
+print("Your Age is:", age)
+
+val = input("Enter some value: ")
+print(type(val), val)
+int("34")
+val = int(input("enter some value: "))
+print(type(val), val)
+
+name = input("Enter your name: ")
+age = input("Enter your Age: ")
+marks = input("Enter your marks: ")
+print("Welcome", name)
+print("Your Age is: ", age)
+print("and Your marks are: ", marks)
+print("You are Passed")
