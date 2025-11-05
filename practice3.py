0efbf821b2126b969b8400687764dc53dadb2f2d
raj4944 <raj4944@gmail.com>
2025-11-05 22:40:10 +0530

    V1

---
+++ practice3.py
@@ -0,0 +1,18 @@
+
+name = input("Enter your Name: ")
+print(name)
+print("length of your name is:",len(name))
+print("your name with first letter in capital is : ", str.capitalize(name))
+
+#WAP to count a occurance of a character in your string
+print("in the following string\nHi, i am your friend")
+print("the letter 'i' appears for: ")
+str = "Hi, i am your friend"
+print(str.count("i"))
+
+#conditional statement
+print("if your age is greater than 18")
+age = 21
+if(age >= 18):
+    print("can vote and apply for certificate")
+    print("you can not vote and can not apply for certificate if you are less than 18 years")
