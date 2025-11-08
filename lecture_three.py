9b2ba8190f64d3851e8cf19bb49f2076ea02db36
raj4944 <raj4944@gmail.com>
2025-11-08 21:23:47 +0530

    V1

---
+++ lecture_three.py
@@ -0,0 +1,34 @@
+marks1 = 55.67
+marks2 = 35
+marks3 = 56
+marks4 = 62.3
+marks = [34,45,56,76,45.5,34.8]
+print(marks)
+print(marks1, marks2, marks3, marks4)
+print(type(marks))
+print(len(marks))
+print(marks[1])
+print(marks[4])
+student = ["rajesh", 45, "Delhi"]
+print(student)
+print(student[0])
+student[0] = "Rohan"
+print(student)
+#list slicing
+marks = [65, 67,34,23,39]
+print(marks[1:4])
+print(marks[-3:-1])
+#list method
+list = [3,4,8,9]
+list.append(4)
+print(list)
+list.sort()
+print(list)
+print(list.sort(reverse=True))
+print(list)
+list1 = ["a", "b", "c", "e", "d"]
+print(list1)
+print(list1.sort(reverse=True))
+print(list1.sort)
+print(list1)
+
