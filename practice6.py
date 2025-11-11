ad496756b2782bb5da5faa58c8056016cff79c09
raj4944 <raj4944@gmail.com>
2025-11-11 22:29:26 +0530

    v1

---
+++ practice6.py
@@ -0,0 +1,7 @@
+#WAP to count the number of students with the "A" grade in the following tuple
+grade = ["C", "D", "A", "A", "E", "A"]
+print(grade.count("A"))
+# WAP to store the value in a list & sort them from "A" to "D"
+#grade = ["C", "D", "A", "A", "E", "A"]
+grade.sort()
+print(grade)
