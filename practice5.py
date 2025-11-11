9cfefed1d2b5fcff8c2f5e75a4181c6027dd4893
raj4944 <raj4944@gmail.com>
2025-11-11 22:08:10 +0530

    v2

--- practice5.py
+++ practice5.py
@@ -1,5 +1,5 @@
 movies = []
-movie1 = input("Enter name of first mivie: ")
+movie1 = input("Enter name of first movie: ")
 #read = movie1
 movie2 = input("Enter name of second movie: ")
 #read = movie2
@@ -9,3 +9,19 @@ movies.append(movie1)
 movies.append(movie2)
 movies.append(movie3)
 print(movies)
+#palindrome of an element
+list1 = [2,4,7,4,2]
+list2 = [2,5,7,9,7,5,2]
+copy_list1 = list1.copy()
+copy_list1.reverse()
+if(copy_list1 == list1):
+    print("palindrome")
+else:
+    print("Not palindrome")
+
+copy_list2 = list2.copy()
+copy_list2.reverse()
+if(copy_list2 == list2):
+    print("palindrome")
+else:
+    print("Not palindrome")
